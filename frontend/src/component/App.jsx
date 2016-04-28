aca1e9f4a86996a4678d583a0154c60d70c8df68
eastpiger <lvwt@shanghaitech.edu.cn>
2016-04-28 17:31:50 +0800

    add frontend

---
+++ frontend/src/component/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { DatePicker } from 'antd';
+
+class App extends Component {
+  render() {
+    return <DatePicker />;
+  }
+}
+
+export default App;
+// export default IndexApp;
+// export default ReserveApp;
+// export default ResultApp;
+
