bf663670d406eb660f4e784e6e8b5d5b32f72dc0
sjhstone <sjhstone@live.com>
2016-04-28 15:43:55 +0800

    draft for model

--- SUTCM_RR/SUTCM_RR_db/models.py
+++ SUTCM_RR/SUTCM_RR_db/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Applicant(models.Model):
+	cellphone_num = models.BigIntegerField()
+	#? Foreign student name
+	name = models.CharField(max_length=30)
+	department = models.PositiveSmallIntegerField()
+
+class Provider(models.Model):
+	name = models.CharField()
+	description = models.TextField()
+
+class Resource(models.Model):
+	category = models.PositiveSmallIntegerField()
+	name = models.CharField()
+	location = models.CharField()
+	capacity = models.PositiveSmallIntegerField()
+	provider = models.ForeignKey('Provider')
+
+class Reservation(models.Model):
+	applicant = models.ForeignKey('Applicant')
+	resource = models.ForeignKey('Resource')
+	status = models.NullBooleanField()
+
