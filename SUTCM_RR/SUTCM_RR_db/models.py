c9f5b5b627af747c4fe4b2ea97136a9ad6d4e8ef
sjhstone <sjhstone@live.com>
2016-04-29 08:21:11 +0800

    draft for views

--- SUTCM_RR/SUTCM_RR_db/models.py
+++ SUTCM_RR/SUTCM_RR_db/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Provider(models.Model):
-	name = models.CharField()
+class Provider(models.Model): # 资源负责人（社团指导老师，电教员……）
+	name = models.CharField(max_length = 20)
 	description = models.TextField(blank = True)
+
 
 class Resource(models.Model):
 	CATEGORIES = (
@@ -20,6 +21,7 @@ class Resource(models.Model):
 	provider = models.ForeignKey(Provider, verbose_name = '负责人')
 	#? Photo and other static resources
 
+
 class Reservation(models.Model):
 # parameters and data
 	DEPARTMENT_CHOICES = (
@@ -27,21 +29,22 @@ class Reservation(models.Model):
 	)
 
 # model field definitions
-	applicant_phone = models.BigIntegerField(unique = True, verbose_name = '手机号')
+	applicant_phone = models.BigIntegerField(unique = True, verbose_name = '申请人手机号')
 	#? Foreign student name
-	applicant_name = models.CharField(max_length = 30, verbose_name = '姓名')
-	applicant_department = models.PositiveSmallIntegerField(verbose_name = '学院')
+	applicant_name = models.CharField(max_length = 30, verbose_name = '申请人姓名')
+	applicant_department = models.PositiveSmallIntegerField(verbose_name = '申请人学院')
+	applicant_sid = models.PositiveSmallIntegerField(verbose_name = '申请人学号')
 
 	resource = models.ForeignKey('Resource')
 	# null for pending, True for accepted, False for rejected.
-	status = models.NullBooleanField()
-	purpose = models.CharField(max_length = 50, blank = True, verbose_name = '使用目的')
+	status = models.NullBooleanField(default = None)
+	purpose = models.CharField(max_length = 50, blank = True, verbose_name = '申请用途')
 	''' switch(resource.category)
 		case room: duration should be 30, 60, 90, ..., 240 min.s and starts from XX:00 or XX:30
 		case consultation: no specific constraint
-	availability_check: begin2 < end && end2 > begin
+	availability_check: begin2 < end1 && end2 > begin1
 	'''
 	time_begin = models.DateTimeField(verbose_name = '开始时间')
 	time_end = models.DateTimeField(verbose_name = '结束时间')
 
-	checkcode = models.CharField(max_length = 255, verbose_name = '查询代码')
+	sn = models.CharField(unique = True, max_length = 5, verbose_name = '查询代码')
--- SUTCM_RR/SUTCM_RR_db/views.py
+++ SUTCM_RR/SUTCM_RR_db/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Provider, Resource, Reservation
+
+def resource_list(request):
+	resource_objects = Resource.objects
+	resources_json = JsonResponse({'resource_list': resource_objects})
+	return resources_json
+
+def resource(request, id):
+	resource
+	resource_json = JsonResponse()
+	return resource_json
+
+def time_check(request, resource_id, time):
+	reservation_records = Reservation.objects.get(resource = resource_id)
+	may_not_available = False
+	for record in reservation_records:
+		if record.time_begin < time[1] & record.time_end > time[0]:
+			if record.status = True:
+				return JsonResponse({'time_available': 0})
+			else:
+				may_not_available = True
+	if may_not_available:
+		return JsonResponse({'time_available': 2})
+	return JsonResponse({'time_available': 1})
+
+def reserve(request, type):
+	if succeeded:
+		reservation_response = JsonResponse({'success': 1, 'reserve_sn': reserve_sn})
+	return reservation_response
+
+
+def result(request, reserve_sn):
+	destination_reservation = Reservation.objects.get(sn = reserve_sn)
+	result_json = JsonResponse({'reservation': destination_reservation})
+	return result_json
