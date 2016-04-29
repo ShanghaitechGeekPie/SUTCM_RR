437ccdbdae8d500505883556e7d6d1b8e89b9675
sjhstone <sjhstone@live.com>
2016-04-30 01:36:24 +0800

    url config refined

--- SUTCM_RR/SUTCM_RR/urls.py
+++ SUTCM_RR/SUTCM_RR/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^resources/(?P<category_id>\d{1})/$', ResourcesList.as_view(), name = 'resources'),
     url(r'^resource/(?P<resource_id>\d+)/$', ResourceInfo.as_view(), name = 'resource'),
-    url(r'^time/(?P<from>\d+)/(?P<to>\d+)/$', TimeCheck.as_view(), name = 'time_check'),
+    url(r'^time/(?P<resource_id>\d+)/(?P<from>\d+)/(?P<to>\d+)/$', TimeCheck.as_view(), name = 'time_check'),
     url(r'^reserve/$', Reserve.as_view(), name = 'reserve'),
     url(r'^result/(?P<reserve_sn>\d|\w{6})/$', Result.as_view(), name = 'result')
 ]
