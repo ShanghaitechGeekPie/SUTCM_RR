b93d9ba54da478093cf467880d34095408bcfad6
sjhstone <sjhstone@live.com>
2016-05-11 08:57:12 +0800

    setting file

--- SUTCM_RR/SUTCM_RR/settings.py
+++ SUTCM_RR/SUTCM_RR/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SUTCM_RR_db'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -61,6 +63,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.core.context_processors.request',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -123,8 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -132,3 +133,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_dev"),
 )
+
+# Django Suit configuration example
+SUIT_CONFIG = {
+    'ADMIN_NAME': '上海中医药大学学生事务中心',
+}
